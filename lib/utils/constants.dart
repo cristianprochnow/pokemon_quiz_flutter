d53df7cd2ef4529cfe9f34b4696a950becd75373
Cristian Prochnow <cristianprochnow@gmail.com>
2023-10-30 23:37:44 -0300

    feat: add constants files

---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+const String pokeApiUrl = "https://pokeapi.co/api/v2/pokemon";
+const String pokemonImageUrl = "https://raw.githubusercontent.com/PokeAPI"
+    "/sprites/master/sprites/pokemon/other/official-artwork";
